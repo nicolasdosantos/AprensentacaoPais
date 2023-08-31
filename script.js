cee44836d0f6ecd2c407d3943c0e6d935ca9bd17
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2023-08-31 16:54:11 -0300

    ------------

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 $("#botaoAtiva").on("click", function(){
     $("#inicio").slideUp("slow", function(){
-        $("#conteudo").fadeIn("slow", function(){
+        $("#proxima").fadeIn("slow", function(){
 
 
 
         })
     })
 })
+
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+});
